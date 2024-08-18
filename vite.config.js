0aa6d90c6d0b6320945c8982c018b671fa9f1fa7
sohyun <sohyun32253@kaywon.ac.kr>
2024-08-18 16:45:50 +0900

    config 수정

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	assetsInclude: ['**/*.PNG', '**/*.png'],  // PNG 파일을 Vite가 올바르게 처리할 수 있도록 포함
 	
   plugins: [
 		vue(),
